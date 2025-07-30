5d8409ad4104d699a53a873db3541fefceb61412
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-30 11:27:08 -0300

    adiciona metadata na 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,12 @@
 //src/app/not-found.tsx
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pagina não encontrada | PetShop",
+  robots: "noindex",
+};
+
 export default function Pagina404() {
   return (
     <section>
