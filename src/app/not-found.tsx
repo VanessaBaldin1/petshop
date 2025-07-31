27de54ad9543abb5cea97390377bcb6414e0a354
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-31 11:34:40 -0300

    adiciona image na page not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import estilos from "./not-found.module.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Pagina não encontrada | PetShop",
@@ -12,6 +13,17 @@ export default function Pagina404() {
   return (
     <section className={estilos.conteudo}>
       <h2>Ops! Página não encontrada!</h2>
+
+      <div className={estilos.imagem404}>
+        <img
+          src="./images/404.svg"
+          alt="Cachorrinho com cara triste e nariz vermelho com o símbolo de interrogação"
+        />
+
+        <p>
+          <Link href="/">Voltar para a página inicial</Link>
+        </p>
+      </div>
     </section>
   );
 }
