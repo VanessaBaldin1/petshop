8eafdb45e839c053aa95c17ee51a01a84d4647f9
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-31 11:44:08 -0300

    adiciona css no container

---
+++ src/components/Container.tsx
@@ -0,0 +1,6 @@
+//src/components/Container.tsx
+import estilos from "./Container.module.css";
+
+export default function Container() {
+  return <div className={estilos.container}>Container</div>;
+}
