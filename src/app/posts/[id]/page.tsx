660cff4992366bb65df5a82ca76792e5e03ed1f6
Vanessa Baldin <vanbaldin08@gmail.com>
2025-08-04 11:03:38 -0300

    resolve desafio

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
 
 // pesquisar depois na documentação do next e na IA sobre Promise no next
 
@@ -10,8 +11,17 @@ type DetalhePostProps = {
 };
 
 export default async function DetalhePost({ params }: DetalhePostProps) {
+  const resultado = await fetch(`http://localhost:2112/posts`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resultado.ok) {
+    throw new Error("Erro ao buscar o post " + resultado.statusText);
+  }
+
+  const posts: Post = await resultado.json();
+
   const { id } = await params;
-  console.log(id);
 
   return (
     <article className={estilos.conteudo}>
