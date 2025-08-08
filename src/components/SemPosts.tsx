37486e50cae9c4657c4924201c04ea6da97e236a
Vanessa Baldin <vanbaldin08@gmail.com>
2025-08-08 09:20:57 -0300

    aplica classname do estilos.semposts

--- src/components/SemPosts.tsx
+++ src/components/SemPosts.tsx
@@ -1,11 +1,14 @@
 //src/components/SemPosts.tsx
 
 import Container from "./Container";
+import estilos from "./SemPosts.module.css";
 
 export default function SemPosts() {
   return (
     <Container>
-      <p>Nenhum post encontrado! Tente novamente mais tarde</p>
+      <p className={estilos.semPosts}>
+        Nenhum post encontrado! Tente novamente mais tarde
+      </p>
     </Container>
   );
 }
