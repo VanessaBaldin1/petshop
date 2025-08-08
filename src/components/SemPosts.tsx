2b268a371b924a0a009f31c0cba7b37474bc7488
Vanessa Baldin <vanbaldin08@gmail.com>
2025-08-08 09:17:54 -0300

    Cria component SemPosts e importa o container

---
+++ src/components/SemPosts.tsx
@@ -0,0 +1,11 @@
+//src/components/SemPosts.tsx
+
+import Container from "./Container";
+
+export default function SemPosts() {
+  return (
+    <Container>
+      <p>Nenhum post encontrado! Tente novamente mais tarde</p>
+    </Container>
+  );
+}
