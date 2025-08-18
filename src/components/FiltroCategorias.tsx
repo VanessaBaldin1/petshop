0664953091ba4a41033684a8e15a1ea4d4f222c3
Vanessa Baldin <vanbaldin08@gmail.com>
2025-08-18 08:49:54 -0300

    Cria componete Categorias com estilos
    ainda sem logica de programação

---
+++ src/components/FiltroCategorias.tsx
@@ -0,0 +1,12 @@
+//src/componets/FiltroCategorias.tsx
+import estilos from "./FiltroCategorias.module.css";
+
+export default function FiltroCategorias() {
+  return (
+    <div className={estilos.categorias}>
+      <button>Categoria A</button>
+      <button>Categoria B</button>
+      <button className={estilos.limpar}>Limpar</button>
+    </div>
+  );
+}
