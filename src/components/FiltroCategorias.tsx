915296154794f618d79345aff02d41325f980e85
Vanessa Baldin <vanbaldin08@gmail.com>
2025-08-18 11:56:24 -0300

    Configura props ao selecionar  a categoria

--- src/components/FiltroCategorias.tsx
+++ src/components/FiltroCategorias.tsx
@@ -1,9 +1,17 @@
 //src/componets/FiltroCategorias.tsx
 import estilos from "./FiltroCategorias.module.css";
 
+//Configurações para os tipos das props
 type FiltroCategoriasProps = {
+  //categorias: tem que ser uma string vinda do array de categorias
   categorias: string[];
+
+  //CategoriaAtiva: pode ser null (quando não tem filtro) ou string (nome da categoria)
   categoriaAtiva: null | string;
+
+  //Esta prop, na verdade, funciona como uma função callback
+  //Ela esta diretamente ligada ao setCategoriaAtiva existente no componente pai(ListaPosts),
+  //Por isso, ela precisa receber como parametro a categoria (ou null) e repassa este dado para setCategoriaAtiva. Usamos void ela não retorna nada.
   aoSelecionar: (categoria: null | string) => void;
 };
 
@@ -14,9 +22,21 @@ export default function FiltroCategorias({
 }: FiltroCategoriasProps) {
   return (
     <div className={estilos.categorias}>
-      <button>Categoria A</button>
-      <button>Categoria B</button>
-      <button className={estilos.limpar}>Limpar</button>
+      {categorias.map((categoria) => (
+        <button
+          className={categoria === categoriaAtiva ? estilos.ativo : ""}
+          key={categoria}
+          onClick={() => aoSelecionar(categoria)}
+        >
+          {categoria}
+        </button>
+      ))}
+
+      {categoriaAtiva && (
+        <button onClick={() => aoSelecionar(null)} className={estilos.limpar}>
+          Limpar
+        </button>
+      )}
     </div>
   );
 }
